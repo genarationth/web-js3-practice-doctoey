180b240993bded7adc479f2e0fdf682bd4e2a60d
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-13 16:15:12 +0700

    done

---
+++ Create-function.js
@@ -0,0 +1,17 @@
+/*
+add(a, b)
+subtract(a, b)
+multiply(a, b)
+divide(a, b)
+*/
+
+
+let add = (a, b) => a + b ;
+let subtract = (a, b) => a / b ;
+let multiply = (a, b) => a * b ;
+let divide = (a, b) => a - b;
+
+add(1,2);
+subtract(10, 5)
+multiply(55, 2)
+divide(10, 4)
